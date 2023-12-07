c0ff40e0110f4b8dfc5519f191f9035a69b7843c
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-12-07 06:19:30 +0000

    route作成終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,38 @@
 Rails.application.routes.draw do
   
-  
-  
-  
-  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+   # なぜ上はusersなのに下はadminなのか？
   
   namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/destroy'
+  get 'top' => 'homes#top', as: 'top'
+  resources :posts, only: [:index, :show, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :genres, only: [:index, :create, :edit, :update]
   end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/update'
-    get 'genres/edit'
-  end
-  namespace :public do
-    get 'genres/show'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/create'
-    get 'posts/destroy'
-    get 'posts/edit'
-    get 'posts/update'
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/chcek'
-    get 'users/withdraw'
-  end
-  namespace :public do
-    get 'homes/top'
-    
-    
-    
-    
-  end
+
+  
+  
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "user/registrations",
   sessions: 'user/sessions'
   }
-  # なぜ上はusersなのに下はadminなのか？
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
- 
+  
+  scope module: :public do
+  root 'homes#top'
+  get 'users/mypage' => 'users#show', as: 'mypage'
+  # users/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+  get 'users/information/edit' => 'users#edit', as: 'edit_information'
+  patch 'users/information' => 'users#update', as: 'update_information'
+  get 'users/chcek' => 'users#chcek', as: 'chcek'
+  put 'users/information' => 'users#update'
+  patch 'users/withdraw' => 'users#withdraw' , as: 'withdraw'
+  
+  
+  resources :genres, only: [:show]
+  resources :comment, only: [:create, :destroy]
+  resources :posts
+  
+  end
 end
