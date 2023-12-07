7f7c5299e0447e0ac90a35ca3b4e4479a7aa32ec
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2023-12-07 03:34:07 +0000

    モデル終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,67 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  
+  
+  
+  
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/destroy'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/update'
+    get 'genres/edit'
+  end
+  namespace :public do
+    get 'genres/show'
+  end
+  namespace :public do
+    get 'comments/create'
+    get 'comments/destroy'
+  end
+  namespace :public do
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/destroy'
+    get 'posts/edit'
+    get 'posts/update'
+    get 'posts/index'
+    get 'posts/show'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/chcek'
+    get 'users/withdraw'
+  end
+  namespace :public do
+    get 'homes/top'
+    
+    
+    
+    
+  end
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+  }
+  # なぜ上はusersなのに下はadminなのか？
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+ 
 end
