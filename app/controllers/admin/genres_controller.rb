45498cd96eee31d7975b4d7fdb0c992d6e55f1ce
YoneiAtsushi <YoneiAtsushi@users.noreply.github.com>
2024-01-29 03:37:15 +0000

    インデント整理

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,5 +1,5 @@
 class Admin::GenresController < ApplicationController
- def index
+  def index
     @genre = Genre.new
     @genres = Genre.all
     # @genre = Genre.new(genre_params)
@@ -16,15 +16,14 @@ class Admin::GenresController < ApplicationController
   end
 
   def update
-    @genre=Genre.find(params[:id])
+    @genre = Genre.find(params[:id])
     @genre.update(genre_params)
     redirect_to admin_genres_path
   end
 
-private
+  private
 
   def genre_params
-     params.require(:genre).permit(:name)
+    params.require(:genre).permit(:name)
   end
-
 end
